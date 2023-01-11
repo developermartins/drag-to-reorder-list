8df0f6c02ccd5648dd4acbbccc33468d8d829f95
marts <martssilvapro@gmail.com>
2023-01-11 14:39:01 -0300

    implements reorderable item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Reorder } from 'framer-motion';
+
 import './App.css';
 
 const App = () => {
+
+  const [items, setItems] = useState(["React", "CSS", "Motion"])
+
   return (
     <section className='content-section'>
-      Oi mãe
+      <Reorder.Group values={ items } onReorder={ setItems }>
+        { items.map(item => (
+          <Reorder.Item key={ item } value={ item }>
+            { item }
+          </Reorder.Item>
+        )) }
+      </Reorder.Group>
     </section>
   );
 };
